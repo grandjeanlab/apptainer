c5e4399914eba33b5933554bb2d1b7418d53e820
Joanes Grandjean <jo@swissisland.ch>
2025-06-02 14:08:32 +0200

    adding nix_vim, remove slim_python3, vscode container

--- nix_nvim/flake.nix
+++ nix_nvim/flake.nix
@@ -7,49 +7,69 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      pkgs = import nixpkgs { system = "x86_64-linux"; allowUnfree = true; };
     in
     {
       packages.x86_64-linux.default = pkgs.singularity-tools.buildImage {
         name = "nvim";
+        singularity = pkgs.apptainer;
+        runAsRoot = ''
+          #!${pkgs.stdenv.shell}
+          ${pkgs.dockerTools.shadowSetup}
+          mkdir -p /.singularity.d/env/
+          echo $APPTAINER_ENVIRONMENT
+          echo "export XDG_CACHE_HOME=~/.hpc-tools-cache" >> /.singularity.d/env/91-custom-environment.sh
+          echo "export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt" >> /.singularity.d/env/91-custom-environment.sh
+          chmod +x /.singularity.d/env/91-custom-environment.sh
+          '';
+        diskSize = 1024 * 1024;
+
         contents = with pkgs; [ 
+
+          nushell
+          busybox
+          cacert
+          curl
           neovim
           fzf
           fd
           lazygit
-          gitMinimal
-          #yazi
-          #ripgrep
+          git
+          yazi
+          ripgrep
 
-          #quarto
+          quarto
 
-          #nodejs-slim
+          nodejs
+          gcc
 
-          #python313
+          (python313.withPackages (ps: with ps; [
+          pandas
+          numpy
+          matplotlib
+          scipy
+          seaborn
+          statsmodels
+          jupyter
+          pyqt5
+          nilearn
+          ]))
           
-          #python313Packages.pandas
-          #python313Packages.numpy
-          #python313Packages.matplotlib
-          #python313Packages.scipy
-          #python313Packages.seaborn
-          #python313Packages.statsmodels
-          #python313Packages.jupyter
-          #python313Packages.pyqt5
-          #python313Packages.nilearn
-
-          #R
-          #rPackages.tidyverse
-          #rPackages.easystats
-          #rPackages.RNifti
-          #rPackages.ggdist
-          #rPackages.cluster
-          #rPackages.wesanderson
-          #rPackages.MetBrewer
-          #rPackages.glue
-          #rPackages.abind
-          #rPackages.ggpubr
-          #rPackages.svglite
-          #rPackages.factoextra
+          (rWrapper.override { packages = with pkgs.rPackages; [
+          tidyverse
+          easystats
+          RNifti
+          ggdist
+          cluster
+          wesanderson
+          MetBrewer
+          glue
+          abind
+          ggpubr
+          svglite
+          factoextra
+          dabestr
+          ];})
 
         ];
         runScript = "${pkgs.neovim}/bin/nvim";
